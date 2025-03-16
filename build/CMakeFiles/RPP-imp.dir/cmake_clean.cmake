2bb2d95ef9eb0765fa29172cc1960553d94497f9
Truong123 <nguyenvantruong1372003@gmail.com>
2025-03-16 22:22:24 +0700

    feature: cmake build

---
+++ build/CMakeFiles/RPP-imp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RPP-imp.dir/src/main.cpp.o"
+  "CMakeFiles/RPP-imp.dir/src/main.cpp.o.d"
+  "RPP-imp"
+  "RPP-imp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RPP-imp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/function/genetic.h
@@ -0,0 +1,6 @@
+#ifndef GENETIC_ALGORITHM_H
+#define GENETIC_ALGORITHM_H
+
+void runGeneticAlgorithm();
+
+#endif // GENETIC_ALGORITHM_H
