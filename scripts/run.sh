966d6b5229c3c5d0a9c6deab10fb91b6e589387a
Truong123 <nguyenvantruong1372003@gmail.com>
2025-04-12 12:36:17 +0700

    ref: ignore .cursor

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,6 @@
 
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B build
 
-
-
 cd RPP-imp
 cd target
 g++ boot.cpp -o server -lpthread -lboost_system
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,5 @@
 
 int main(int argc, char *argv[]) {
   runApp();
-  
   return 0;
 }
