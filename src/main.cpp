bba35cabc2b9e09a51f89ca9c7e6d1b185cbb959
Truong123 <nguyenvantruong1372003@gmail.com>
2025-04-13 14:36:26 +0700

    feat: add past repo reference

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
-#include "app.h"
-#include "utils.h"
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-  runApp();
-  return 0;
+#include "app.h"
+#include "utils.h"
+
+int main(int argc, char *argv[])
+{
+    runApp();
+    return 0;
 }
